b901460c70d16895200cf262b4c38f24caf8cd0f
„jasminstein“ <jasmin.stein@outlook.de>
2026-03-02 13:48:37 +0100

    delete

--- old/testexp.py
+++
@@ -1,9 +0,0 @@
-from psychopy import prefs, sound, core
-
-prefs.hardware['audioLib'] = ['ptb']  # fallback chain
-prefs.hardware['audioDevice'] = 'Externe Kopfhörer'  # comment out temporarily
-prefs.hardware['audioLatencyMode'] = 3
-
-testSound = sound.Sound('C', secs=1.0)
-testSound.play()
-core.wait(1.5)
